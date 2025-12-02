96563c5c88bcaa5ed85526cd468ae9fd428922d8
finneganblack358-max <finneganblack358@gmail.com>
2025-12-02 18:55:51 -0500

    Events page and route

--- api/routes.py
+++ api/routes.py
@@ -5,3 +5,7 @@ api = Blueprint("api", __name__)
 @api.route("/")
 def index():
     return render_template("index.html")
+
+@api.route("/events")
+def events():
+    return render_template("events.html")
